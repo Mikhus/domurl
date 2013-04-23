ebf1ee2f5fa1b0139f9a94f41c6fc3fbe840a7fb
mstadnyk <mykhailo.stadnyk@globallogic.com>
2013-04-23 23:15:55 +0300

    Fix problem with unsetting query params via assigning a null value

--- url.js
+++ url.js
@@ -108,7 +108,7 @@
 						value = match[3] ? decode(match[3]) : ''
 					;
 
-					if (this[key] !== undefined) {
+					if (this[key] != null) {
 						if (!(this[key] instanceof Array)) {
 							this[key] = [this[key]];
 						}
